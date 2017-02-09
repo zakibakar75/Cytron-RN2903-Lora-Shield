805b9b7510a9f445210c04ad80b78dd611bc5dc3
Ahmad Zaki <zaki.bm@gmail.com>
2017-02-10 00:41:08 +0800

    Latest changes
    Include sensor reading and convert it to char then put into the char array for transmission

--- RN2903_LoRaWAN_Zaki.ino
+++ RN2903_LoRaWAN_Zaki.ino
@@ -2,20 +2,27 @@
 
 #include "LoRaWAN.h"
 #include <SoftwareSerial.h>
+#include<dht.h>
 
 #define ON 1
 #define OFF 0
+#define MAX_HEX_DIGIT 2
+#define DHT11_PIN 8
+
 
 //************** Device parameters for registration *****************
-char DEVICE_ADDR[] = "26021BF2";  //4 bytes required
-char NWK_SESSION_KEY[] = "C66C9E1F3D7D6E7DE9A84A7E94704C3F"; // 16 bytes required
-char APP_SESSION_KEY[] = "CAA5C87D0A6A7E82CBA09E37F09EA117"; //16 bytes required
+char DEVICE_ADDR[] = "2602125E";  //4 bytes required 
+char NWK_SESSION_KEY[] = "06E635B6E76E02DD8BFBFC0B268F3199"; // 16 bytes required
+char APP_SESSION_KEY[] = "745919635797A64960F3C045EF6DB336"; //16 bytes required
 char APP_KEY[] = "0AA5AF9F0B74FF519D7FB0FF57D3EB9C"; //16 bytes required
-char APP_EUI[] =  "70B3D57EF0001D5E";  //8 bytes required
-
-char txData[]="01020304050";      //sample data to be transmitted - must be hexadecimal values
+char APP_EUI[] =  "70B3D57EF00038C7";  //8 bytes required
 
 LoRaWAN LoRaWAN(2,3); // Software Serial RX, TX   ** Set to 10, 11 for Mega2560 boards, Set to 5, 4 for Uno 
+dht DHT;
+
+/* Function Prototype */
+void ConvertResult (uint8_t passedResult, char* buf);
+void AssignToCharArray(int startArrayNum, char* txData, char* tempData);
 
 
 void setup() {
@@ -41,19 +48,68 @@ LoRaWAN.LoRaShowConfig();    //display current module configuration for debuggin
 LoRaWAN.LoRaJoinABP ();       //Connect with Access By Personalization (ABP) networks
 //LoRaWAN.LoRaJoinOTAA ();    //Connect with Over The Air Activation (OTAA) networks 
 delay(1000);
+
 }
 
+
 void loop() {
- 
+
+  int i;
+  String str;
+  char tempData[3];
+  char txData[7];   //data to be transmitted - must be hexadecimal values
+  uint8_t result,chk,humidity,temperature;
+  
   //*************** Display received data from LoRaWAN 
   while (LoRaWAN.loraSerial.available()) {
     Serial.write(LoRaWAN.loraSerial.read());
   }
-            
-  //*************** Display then transmit data to LoRaWAN
+  
+  chk = DHT.read11(DHT11_PIN);
+  Serial.println("Humidity " );
+  Serial.println(DHT.humidity, 1);
+  Serial.println("Temperature ");
+  Serial.println(DHT.temperature, 1);
+  
+  /************************** reading 1 **************************/
+  result=DHT.temperature;
+  ConvertResult(result, tempData);
+  AssignToCharArray(0, txData, tempData);   
+  /***************************************************************************/
+ 
+  /************************* reading 2 ****************************/
+  result=DHT.humidity;
+  ConvertResult(result,tempData);
+  AssignToCharArray(2, txData, tempData);
+
+  result=0xAA;
+  ConvertResult(result,tempData);
+  AssignToCharArray(4, txData, tempData);
+  /***************************************************************************/
+  
+  txData[6] = '\0';
+              
+  //*************** Display then transmit data to LoRaWAN ********************/
   Serial.print("\nSending: ");Serial.println(txData);
   LoRaWAN.LoRaTransmit(1, 30, txData);   //type 0 = Confirmed, 1 = Unconfirmed / port number 1 to 223 / data payload
+   
   delay(10000);
-
+  
 }
 
+void ConvertResult(uint8_t passedResult, char* buffer)
+{
+  String str;
+  str=String(passedResult,HEX);
+  str.toCharArray(buffer,3);
+}
+
+void AssignToCharArray(int startArrayNum, char* txData, char* tempData)
+{  
+  int i;
+  
+  for(i=startArrayNum; i<(MAX_HEX_DIGIT+startArrayNum); i++)
+  {
+    txData[i] = tempData[i-startArrayNum];
+  }
+}
